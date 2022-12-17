482cffe6f1f8c0288e9d3c02ff8e84736209208f
DJTX97 <the.10.of.one@gmail.com>
2022-12-17 14:21:55 +0200

    Mobile Card Update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PIMG from "../assets/images/placeholder.png";
 import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
-// import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Card = ({ name, image, actors, plot, year, genres }) => {
   const navigate = useNavigate();
@@ -18,10 +17,26 @@ const Card = ({ name, image, actors, plot, year, genres }) => {
     });
   };
 
+  const [touchedCard, setTouchedCard] = useState(false);
+
+  const touchCard = () => {
+    setTouchedCard(!touchedCard);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      touchedCard && setTouchedCard(false);
+    }, 200);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [touchedCard]);
+
   return (
     <div
-      className={`w-[250px] h-[350px] grid grid-rows-auto justify-items-center m-5 rounded-3xl bg-black/40 shadow-lg hover:scale-105 hover:shadow-red-600 cursor-pointer transition-all`}
+      className={`w-[250px] h-[350px] grid grid-rows-auto justify-items-center m-5 rounded-3xl bg-black/40 shadow-lg hover:scale-105 hover:shadow-red-600 cursor-pointer transition-all ${touchedCard && "transition-none scale-105 shadow-red-600"}`}
       onClick={seeMovie}
+      onTouchStart={touchCard}
     >
       <div className="translate-y-12 translate-x-[-4.5rem] bg-gray-500/90 text-white font-bold px-1 h-6">
         {year}
