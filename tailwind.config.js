320c1a0c8c8af0e7fb69bab75626931f041c296d
DJTX97 <the.10.of.one@gmail.com>
2022-12-26 14:40:53 +0200

    Refresh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'banner': "url('./assets/images/bg.jpg')",
-      }
+        banner: "url('./assets/images/bg.jpg')",
+      },
     },
   },
   plugins: [],
-}
+};
